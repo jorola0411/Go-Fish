ae35989925b889e153435542b96c42926d11938a
jorola0411 <jorola@my.bcit.ca>
2025-02-28 18:56:31 -0800

    Added go fish for player/cpu

--- gofish/src/gameplay/Turns.jsx
+++ gofish/src/gameplay/Turns.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import useInitGame from './startGame';
 
 function Turns() {
@@ -13,16 +13,31 @@ function Turns() {
     const playerTurn = (requestedCardValue) => {
         const matchingCard = cpuHand.find(card => card.value === requestedCardValue);
         if (matchingCard) {
-                setPlayerHand(prevHand => [...prevHand, matchingCard]);
-                setCpuHand(prevHand => prevHand.filter(card => card.value !== requestedCardValue));
-                changeTurn();
-            }
-            else{
-                console.log('Go Fish!');
-                goFish(playerHand);
+            setPlayerHand(prevHand => [...prevHand, matchingCard]);
+            setCpuHand(prevHand => prevHand.filter(card => card.value !== requestedCardValue));
+            changeTurn();
+        }
+        else {
+            console.log('Go Fish!');
+            goFish(playerHand);
 
-                changeTurn();
-            }
+            changeTurn();
+        }
+    }
+
+
+
+    const playerGoFish = async () => {
+        const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        const data = await response.json();
+        setPlayerHand(prevHand => [...prevHand, data.cards[0]]);
+        setRemainingCards(data.remaining);
+    }
+    const cpuGoFish = async () => {
+        const response = await fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+        const data = await response.json();
+        setCpuHand(prevHand => [...prevHand, data.cards[0]]);
+        setRemainingCards(data.remaining);
     }
 }
-export default Turns;
+    export default Turns;
